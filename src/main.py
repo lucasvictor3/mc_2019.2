5c5f6e5741ef81e3530ce16e2d91df0603624306
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2019-10-30 11:28:22 -0300

    plota graficos

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
 
 def q1():
+
 	raw_dataset = pd.read_csv('../data/enade2017_ufcg.csv')
 
 	dataset = raw_dataset.copy()
-	print(dataset.tail())
+    	sns.relplot(x="QE_I08", y="NT_GER", kind="line",ci=None, data=dataset)
+        sns.relplot(x="QE_I23", y="NT_GER", kind="line",ci=None, data=dataset)
+        plt.show()        
 
 q1()

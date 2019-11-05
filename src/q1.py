43629e3c81a3ce81c82ce5359e18fb594e0c730a
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-11-05 20:36:52 -0300

    Reorder methods

--- src/q1.py
+++ src/q1.py
@@ -78,41 +78,6 @@ def onde_com_quem_mora_ANOVA():
 
     print('Onde e com Quem Mora ANOVA:\n\tP-Valor: %.16f' % p)
 
-def renda_familiar_total():
-	di = {'A': u'até\nR\$ 1.405,50',
-		  'B': 'R\$ 1.405,51\na\n R\$ 2.811,00',
-		  'C': 'R\$ 2.811,01\na\n R\$ 4.216,50',
-		  'D': 'R\$ 4.216,51\na\n R\$ 5.622,00',
-		  'E': 'R\$ 5.622,01\na\n R\$ 9.370,00',
-		  'F': 'R\$ 9.370,01\na\n R\$ 28.110,00',
-		  'G': 'mais de\n R\$ 28.110,00'}
-
-	df = dataset.replace({'QE_I08': di})
-
-	plt.figure(figsize=(12, 8))
-
-	ax = sns.barplot(x='QE_I08', y='NT_GER', data=df, ci=None, order=[
-					 di['A'], di['B'], di['C'], di['D'], di['E'],
-					 di['F'], di['G']])
-	ax.set_xlabel('Renda Familiar Total', fontsize=14, labelpad=20)
-	ax.set_ylabel('Nota Geral', fontsize=14, labelpad=20)
-
-	plt.tight_layout()
-	plt.ylim(40, None)
-	plt.show()
-
-def renda_familiar_total_ANOVA():
-    df = dataset.pivot(columns='QE_I08',values='NT_GER')
-    F, p = stats.f_oneway(df[df['A'].notnull()]['A'],
-                          df[df['B'].notnull()]['B'],
-                          df[df['C'].notnull()]['C'],
-                          df[df['D'].notnull()]['D'],
-                          df[df['E'].notnull()]['E'],
-                          df[df['F'].notnull()]['F'],
-                          df[df['G'].notnull()]['G'])
-
-    print('Renda Familiar Total ANOVA:\n\tP-Valor: %.16f' % p)
-
 def escolarizacao_pai():
 	di = {'A': 'Nenhuma',
 		  'B': u'Ensino Fundamental:\n1ª a 4ª série',
@@ -179,13 +144,48 @@ def escolarizacao_mae_ANOVA():
 
     print('Escolarização da Mãe ANOVA:\n\tP-Valor: %.16f' % p)
 
+def renda_familiar_total():
+	di = {'A': u'até\nR\$ 1.405,50',
+		  'B': 'R\$ 1.405,51\na\n R\$ 2.811,00',
+		  'C': 'R\$ 2.811,01\na\n R\$ 4.216,50',
+		  'D': 'R\$ 4.216,51\na\n R\$ 5.622,00',
+		  'E': 'R\$ 5.622,01\na\n R\$ 9.370,00',
+		  'F': 'R\$ 9.370,01\na\n R\$ 28.110,00',
+		  'G': 'mais de\n R\$ 28.110,00'}
+
+	df = dataset.replace({'QE_I08': di})
+
+	plt.figure(figsize=(12, 8))
+
+	ax = sns.barplot(x='QE_I08', y='NT_GER', data=df, ci=None, order=[
+					 di['A'], di['B'], di['C'], di['D'], di['E'],
+					 di['F'], di['G']])
+	ax.set_xlabel('Renda Familiar Total', fontsize=14, labelpad=20)
+	ax.set_ylabel('Nota Geral', fontsize=14, labelpad=20)
+
+	plt.tight_layout()
+	plt.ylim(40, None)
+	plt.show()
+
+def renda_familiar_total_ANOVA():
+    df = dataset.pivot(columns='QE_I08',values='NT_GER')
+    F, p = stats.f_oneway(df[df['A'].notnull()]['A'],
+                          df[df['B'].notnull()]['B'],
+                          df[df['C'].notnull()]['C'],
+                          df[df['D'].notnull()]['D'],
+                          df[df['E'].notnull()]['E'],
+                          df[df['F'].notnull()]['F'],
+                          df[df['G'].notnull()]['G'])
+
+    print('Renda Familiar Total ANOVA:\n\tP-Valor: %.16f' % p)
+
 estado_civil()
 estado_civil_ANOVA()
 onde_com_quem_mora()
 onde_com_quem_mora_ANOVA()
-renda_familiar_total()
-renda_familiar_total_ANOVA()
 escolarizacao_pai()
 escolarizacao_pai_ANOVA()
 escolarizacao_mae()
 escolarizacao_mae_ANOVA()
+renda_familiar_total()
+renda_familiar_total_ANOVA()

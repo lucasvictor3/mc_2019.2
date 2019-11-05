031d55d2b51375d7dd40c42acd338e45df413e83
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-11-05 20:38:04 -0300

    Reorder methods

--- src/q1.py
+++ src/q1.py
@@ -44,40 +44,6 @@ def estado_civil_ANOVA():
 
     print('Estado Civil ANOVA:\n\tP-Valor: %.16f' % p)
 
-def onde_com_quem_mora():
-	di = {'A': 'Casa ou apartamento\nsozinho',
-		  'B': 'Casa ou apartamento\ncom pais e/ou parentes',
-		  'C': 'Casa ou apartamento\ncom cônjuge e/ou filhos',
-		  'D': 'Casa ou apartamento\ncom outras pessoas\n(incluindo república)\
-',
-		  'E': 'Em alojamento\nuniversitário da\nprópria instituição',
-          'F': 'Em outros tipos de\nhabitação individual\nou coletiva\n(hotel,\
- hospedaria,\npensão ou outro)'}
-
-	df = dataset.replace({'QE_I06': di})
-
-	plt.figure(figsize=(12, 8))
-
-	ax = sns.barplot(x='QE_I06', y='NT_GER', data=df, ci=None, order=[di['A'],
-                     di['B'], di['C'], di['D'], di['E'], di['F']])
-	ax.set_xlabel('Onde e com quem Mora', fontsize=14, labelpad=20)
-	ax.set_ylabel('Nota Geral', fontsize=14, labelpad=20)
-
-	plt.tight_layout()
-	plt.ylim(40, None)
-	plt.show()
-
-def onde_com_quem_mora_ANOVA():
-    df = dataset.pivot(columns='QE_I06',values='NT_GER')
-    F, p = stats.f_oneway(df[df['A'].notnull()]['A'],
-                          df[df['B'].notnull()]['B'],
-                          df[df['C'].notnull()]['C'],
-                          df[df['D'].notnull()]['D'],
-                          df[df['E'].notnull()]['E'],
-                          df[df['F'].notnull()]['F'])
-
-    print('Onde e com Quem Mora ANOVA:\n\tP-Valor: %.16f' % p)
-
 def escolarizacao_pai():
 	di = {'A': 'Nenhuma',
 		  'B': u'Ensino Fundamental:\n1ª a 4ª série',
@@ -179,13 +145,47 @@ def renda_familiar_total_ANOVA():
 
     print('Renda Familiar Total ANOVA:\n\tP-Valor: %.16f' % p)
 
+def onde_com_quem_mora():
+	di = {'A': 'Casa ou apartamento\nsozinho',
+		  'B': 'Casa ou apartamento\ncom pais e/ou parentes',
+		  'C': 'Casa ou apartamento\ncom cônjuge e/ou filhos',
+		  'D': 'Casa ou apartamento\ncom outras pessoas\n(incluindo república)\
+',
+		  'E': 'Em alojamento\nuniversitário da\nprópria instituição',
+          'F': 'Em outros tipos de\nhabitação individual\nou coletiva\n(hotel,\
+ hospedaria,\npensão ou outro)'}
+
+	df = dataset.replace({'QE_I06': di})
+
+	plt.figure(figsize=(12, 8))
+
+	ax = sns.barplot(x='QE_I06', y='NT_GER', data=df, ci=None, order=[di['A'],
+                     di['B'], di['C'], di['D'], di['E'], di['F']])
+	ax.set_xlabel('Onde e com quem Mora', fontsize=14, labelpad=20)
+	ax.set_ylabel('Nota Geral', fontsize=14, labelpad=20)
+
+	plt.tight_layout()
+	plt.ylim(40, None)
+	plt.show()
+
+def onde_com_quem_mora_ANOVA():
+    df = dataset.pivot(columns='QE_I06',values='NT_GER')
+    F, p = stats.f_oneway(df[df['A'].notnull()]['A'],
+                          df[df['B'].notnull()]['B'],
+                          df[df['C'].notnull()]['C'],
+                          df[df['D'].notnull()]['D'],
+                          df[df['E'].notnull()]['E'],
+                          df[df['F'].notnull()]['F'])
+
+    print('Onde e com Quem Mora ANOVA:\n\tP-Valor: %.16f' % p)
+
 estado_civil()
 estado_civil_ANOVA()
-onde_com_quem_mora()
-onde_com_quem_mora_ANOVA()
 escolarizacao_pai()
 escolarizacao_pai_ANOVA()
 escolarizacao_mae()
 escolarizacao_mae_ANOVA()
+onde_com_quem_mora()
+onde_com_quem_mora_ANOVA()
 renda_familiar_total()
 renda_familiar_total_ANOVA()
